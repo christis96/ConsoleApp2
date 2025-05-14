d5e8a2e9cf1ddbc81f5b5b4d82e5f3321d2904f7
christian <christian.ulstein@gmail.com>
2025-05-14 11:50:54 +0200

    Converter

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+    Console.WriteLine("NOK til Euro");
+    Console.WriteLine("NOK til USD");
+
+
+
+    string valg = Convert.ToString(Console.ReadLine());
+    double belop = Convert.ToDouble(Console.ReadLine());
+    string fra = "", til = "";
+    double resultat = 0;
+
+
+    switch (valg)
+            {
+                case "1": 
+                    resultat = belop / 11.56;
+                    Console.WriteLine($"Resultat: {resultat}");
+                    break;
+                case "2":
+                    resultat = belop / 10.29;
+                    Console.WriteLine($"Resultat: {resultat}");
+                    break;
+                default: Console.WriteLine("Ugyldig svar");
+                    break;
+            }
+
+  
